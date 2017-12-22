ab9164eaeb828f7cea0739ef1e174bdd20179bcc
Marcus Florentin <fmarcus28@gmx.fr>
2017-12-22 22:26:32 +0100

    Initial Commit

---
+++ Tools42/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Tools42
+//
+//  Created by Marcus Florentin on 22/12/2017.
+//  Copyright © 2017 Marcus Florentin. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
